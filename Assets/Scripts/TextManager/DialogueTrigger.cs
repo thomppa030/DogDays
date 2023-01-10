cc0107ab24388b5a169c8be298f9ada4437b9e51
Matthias Voigt <contact@mvgamedev.com>
2023-01-10 18:45:54 +0100

    ADD DialogueManager; DialogueTrigger; Dialogue created Testscene for DialogueManager

---
+++ Assets/Scripts/TextManager/DialogueTrigger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+    public void TriggerDialogue()
+    {
+        DialogueManager.instance.ChangeTextstate(DialogueManager.TextState.onDisplay, dialogue);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TriggerDialogue();
+        }
+    }
+}
