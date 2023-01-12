4c36e2d9dc29e6f727929f430b61172811c9ef31
Kay Priesnitz <kpriesnitz@outlook.de>
2023-01-12 19:29:42 +0100

    ADD Playercontroller without Cameracollisiondetection

---
+++ Assets/Scripts/PlayerController/PlayerStateMachine/PlayerStateBase.cs
@@ -0,0 +1,11 @@
+﻿
+
+public abstract class PlayerStateBase : State
+{
+    protected PlayerStateMachine PlayerStateMachine;
+    
+    public PlayerStateBase(PlayerStateMachine playerStateMachine)
+    {
+        PlayerStateMachine = playerStateMachine;
+    }
+}
---
+++ Assets/Scripts/PlayerController/PlayerStateMachine/PlayerStateMachine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerStateMachine : StateMachine
+{
+    [field: SerializeField] public InputReader InputReader { get; private set; }
+    [field: SerializeField] public CharacterController Controller { get; private set; }
+    [field: SerializeField] public Animator Animator { get; private set; }
+    [field: SerializeField] public float MovementSpeed { get; private set; }
+    [field: SerializeField] public float RotationDampening { get; private set; }
+    [field: SerializeField] public Transform MainCameraTransform { get; private set; }
+    
+    private GameObject _camera;
+
+    private void Awake()
+    {
+        if (_camera == null)
+        {
+            _camera = GameObject.FindGameObjectWithTag("MainCamera");
+        }
+    }
+
+    private void Start()
+    {
+        MainCameraTransform = _camera.transform;
+        
+        SwitchState(new PlayerTestState(this));
+    }
+}
