8729a83f1a30ab54a5815d09c124186ead635478
Matthias Voigt <contact@mvgamedev.com>
2023-01-19 19:17:54 +0100

    UPDATE TextManager; improved dialogue System with actions

--- Assets/Scripts/TextManager/Dialogue.cs
+++ Assets/Scripts/TextManager/Dialogue.cs
@@ -5,24 +5,34 @@ using UnityEngine;
 [HelpURL("https://docs.unity3d.com/Packages/com.unity.ugui@1.0/manual/StyledText.html")]
 public class Dialogue : ScriptableObject
 {
-    [Header("Default Text")]
-    [TextArea(3,10)]
-    public List<string> ger_default = new List<string>();
-    [TextArea(3, 10)]
-    public List<string> eng_default = new List<string>();
+    [SerializeField] private string Title = "default Title";
 
-    [Header("Unlocked Text")]
+    [field: SerializeField] public bool startOnAwake { get; set; }
+    [field: SerializeField] public Action[] Actions { get; set; }
+    [Space]
+    [Header("Sentences")]
     [TextArea(3, 10)]
-    public List<string> ger_unlocked = new List<string>();
+    public List<string> ger_sentences = new List<string>();
     [TextArea(3, 10)]
-    public List<string> eng_unlocked = new List<string>();
+    public List<string> eng_sentences = new List<string>();
+    [Space]
+    [Header("TextData References")]
+    public List<Dialogue> textToUnlock = new List<Dialogue>();
+    public List<Dialogue> textToEnable = new List<Dialogue>();
+    public List<Dialogue> textToDisable = new List<Dialogue>();
 
-    [Header("Unlocking Dialogues for")]
-    public List<Dialogue> UnlockableDialogues = new List<Dialogue>();
-    public bool newTextUnlocked = false;
+
+    public enum Action
+    {
+        nextSentence,
+        enableTextDisplay,
+        disableTextDisplay,
+        wait,
+        fadeIn,
+        fadeOut,
+        shakeCamera,
+        endDialogue
+    }
 
     
-
-
-
 }
--- Assets/Scripts/TextManager/DialogueTrigger.cs
+++ Assets/Scripts/TextManager/DialogueTrigger.cs
@@ -1,16 +1,98 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
 
 public class DialogueTrigger : MonoBehaviour
 {
-    public Dialogue dialogue;
+    public TriggerState triggerState = TriggerState.enabled;
+
+    public Dialogue defaultDialogue;
+    public Dialogue unlockedDialogue;
+
+    private Dialogue displayedDialogue;
+    BoxCollider bc;
+
+    private void OnEnable()
+    {
+        DialogueManager.EnableTextTrigger += EnableTrigger;
+        DialogueManager.DisableTextTrigger += DisableTrigger;
+        DialogueManager.UnlockText += UnlockText;
+    }
+
+    private void OnDisable()
+    {
+        DialogueManager.EnableTextTrigger -= EnableTrigger;
+        DialogueManager.DisableTextTrigger -= DisableTrigger;
+        DialogueManager.UnlockText -= UnlockText;
+    }
 
     private void Start()
     {
         //Setting layer to Dialogue Layer;
         gameObject.layer = 6;
+        bc = GetComponent<BoxCollider>();
+
+        displayedDialogue = defaultDialogue;
+        SetTriggerState(triggerState);
+
+        if (displayedDialogue.startOnAwake) TriggerDialogue();
+
     }
     public void TriggerDialogue()
     {
-        DialogueManager.instance.ChangeTextstate(DialogueManager.TextState.onDisplay, dialogue);
+        DialogueManager.instance.ChangeTextstate(DialogueManager.TextState.onDisplay, displayedDialogue);
+    }
+
+    public enum TriggerState
+    {
+        hidden,
+        enabled,
+        setDefaultText,
+        setUnlockedText
+    }
+
+    public void SetTriggerState(TriggerState tS)
+    {
+        switch (tS)
+        {
+            case TriggerState.enabled:              
+                bc.enabled = true;
+                triggerState = tS;
+                break;
+            case TriggerState.hidden:
+                bc.enabled = false;
+                triggerState = tS;
+                break;
+            case TriggerState.setDefaultText:
+                displayedDialogue = defaultDialogue;
+                triggerState = tS;
+                break;
+            case TriggerState.setUnlockedText:
+                displayedDialogue = unlockedDialogue;
+                triggerState = tS;
+                if (displayedDialogue.startOnAwake) 
+                    TriggerDialogue();
+                break;
+        }
+    }
+
+    private void EnableTrigger(List<Dialogue> dList)
+    {
+        if (dList.Contains(displayedDialogue))
+            SetTriggerState(TriggerState.enabled);
+    }
+    private void DisableTrigger(List<Dialogue> dList)
+    {
+        if (dList.Contains(displayedDialogue))
+            SetTriggerState(TriggerState.hidden);
+    }
+    private void UnlockText(List<Dialogue> dList)
+    {
+        if (dList.Contains(unlockedDialogue))
+        {
+            SetTriggerState(TriggerState.setUnlockedText);
+        }
+            
     }
 }
