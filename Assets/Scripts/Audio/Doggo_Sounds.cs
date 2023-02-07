b283cb84232decef39c08cc962974e00a027b5bc
Lenja Raschke <lenjaraschke@gmx.de>
2023-02-07 18:04:39 +0100

    ADD Doggo Animation Sounds Script
    updated doggo prefab

---
+++ Assets/Scripts/Audio/Doggo_Sounds.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doggo_Sounds : MonoBehaviour
+{
+
+    [SerializeField]
+    private AudioClip[] StepClips; 
+    [SerializeField]
+    private AudioClip[] BarkClips;
+    [SerializeField]
+    private AudioClip scratch; 
+    [SerializeField]
+    private AudioClip[] WhineClips;
+    
+
+    private AudioSource audioSource;
+    private Animator animation;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        animation = GetComponent<Animator>();
+    }
+
+    public void Step()
+    {
+        if (animation.GetFloat("Speed_f") > 0.1 && animation.GetFloat("Speed_f") < 0.6)
+        {
+            AudioClip clip = GetRandomClip(StepClips);
+            audioSource.PlayOneShot(clip);
+        }
+        
+    }
+
+    public void StepRun()
+    {
+        if (animation.GetFloat("Speed_f") >= 0.6)
+        {
+            AudioClip clip = GetRandomClip(StepClips);
+            audioSource.PlayOneShot(clip);
+        }
+    }
+    
+    public void Bark()
+    {
+        AudioClip clip = GetRandomClip(BarkClips);
+            audioSource.PlayOneShot(clip);
+        
+    }
+
+    public void Scratching()
+    {
+        audioSource.PlayOneShot(scratch);
+    }
+
+    public void Whining()
+    {
+        AudioClip clip = GetRandomClip(WhineClips);
+        audioSource.PlayOneShot(clip);
+    }
+
+    private AudioClip GetRandomClip(AudioClip[] AudioArray)
+    {
+        return AudioArray[UnityEngine.Random.Range(0, AudioArray.Length)];
+    }
+}
