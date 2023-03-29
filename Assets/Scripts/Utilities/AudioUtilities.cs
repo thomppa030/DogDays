3733a846291fc67b386ce8fc8b9654268a818c5d
Kay Priesnitz <kpriesnitz@outlook.de>
2023-03-29 17:12:00 +0200

    UPDATE Removed unused Utility Class

--- Assets/Scripts/Utilities/AudioUtilities.cs
+++ Assets/Scripts/Utilities/AudioUtilities.cs
@@ -14,22 +14,3 @@ public class AudioUtilities : MonoBehaviour
         return AudioArray[UnityEngine.Random.Range(0, AudioArray.Length)];
     }
 }
-
-public class WaitingUtilities : MonoBehaviour
-{
-    private void Start()
-    {
-        InteractionManager.Instance.OnDialogueWait += Wait;
-    }
-
-    public static IEnumerator IWait(float waitTime)
-    {
-        Debug.Log($"Wait for {waitTime} seconds.");
-        yield return new WaitForSeconds(waitTime);
-    }
-
-    private void Wait(float waitTime)
-    {
-        StartCoroutine(IWait(waitTime));
-    }
-}
